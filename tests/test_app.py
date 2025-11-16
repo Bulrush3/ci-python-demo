41fdbd7c04bdf541009376d8e1ee4b95a0cd8a22
Bulrush3 <mike10.09.2000@yandex.ru>
2025-11-16 21:15:01 +0500

    Add build artifact step

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 import os
+
 import sys
 
 # Добавляем корень проекта в sys.path
@@ -6,8 +7,10 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from app import add
 
+
 def test_add_positive():
-    assert add(2, 3) == 6 
+    assert add(2, 3) == 5
+
 
 def test_add_negative():
     assert add(-1, -2) == -3
